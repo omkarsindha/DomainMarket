0118cd9cb2e1a5a7a0718a935091ad9a0fe4a4a0
Omkarsinh Sindha <sindhao@sheridancollege.ca>
2025-03-31 23:49:13 -0400

    Sign up and Login Page Working

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -14,7 +14,7 @@ const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="HomeTab"
+        initialRouteName="Start"
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="Start" component={LandingScreen} />
