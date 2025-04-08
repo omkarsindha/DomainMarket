d5778493bd4c1eb3e846010ba3dbd1029098b79a
Omkarsinh Sindha <sindhao@sheridancollege.ca>
2025-04-07 20:46:05 -0400

    Upate user detail page and added checkout page

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -7,14 +7,14 @@ import HomeTab from "../tabs/HomeTab";
 import LandingScreen from "../screens/LandingScreen";
 import SignUpScreen from "../screens/SignUpScreen";
 import DomainBuyScreen from "../tabs/DomainBuyScreen";
-
+import CheckoutScreen from "../tabs/CheckoutScreen";
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Login"
+        initialRouteName="DomainBuy"
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="Start" component={LandingScreen} />
@@ -22,7 +22,7 @@ const AppNavigator = () => {
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="SignUp" component={SignUpScreen} />
         <Stack.Screen name="DomainBuy" component={DomainBuyScreen} />
-
+        <Stack.Screen name="CheckoutScreen" component={CheckoutScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
